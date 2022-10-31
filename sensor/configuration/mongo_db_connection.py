4b21fcae791d3d79fe887e55c3d840dbb727e28d
Rahul404R <ragulr404@gmail.com>
2022-11-01 00:13:18 +0530

    db conn update

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,15 +1,19 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
+# from sensor.constant.env_variable import MONGODB_URL_KEY
 import certifi
+import os
 ca = certifi.where()
 
 class MongoDBClient:
     client = None
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
+
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://ragul:Password123@cluster0.ughfqwr.mongodb.net/?retryWrites=true&w=majority"
-                MongoDBClient.client = pymongo.MongoDBClient(mongo_db_url,tlsCAFile=ca)
+                # mongo_db_url = os.getenv(MONGODB_URL_KEY)
+               mongo_db_url  = "mongodb+srv://ragul:Password123@cluster0.ughfqwr.mongodb.net/?retryWrites=true&w=majority"
+               MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
