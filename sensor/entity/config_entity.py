f53d607a2e8e8c31489bb5be65d707b28136245e
Rahul404R <ragulr404@gmail.com>
2022-11-09 23:50:23 +0530

    Data trainer & Evaluation Pipeline updated

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 import os
 from sensor.constant  import training_pipeline
@@ -29,38 +28,40 @@ class DataIngestionConfig:
             self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
 
 
+
+
+
 class DataValidationConfig:
-        def __init__(self,training_pipeline_config:TrainingPipelineConfig):  
-              self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
 
-              self.valid_data_dir:str = os.path.join(self.data_validation_dir,training_pipeline.DATA_VALIDATION_VALID_DIR)
 
-              self.invalid_data_dir:str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
+        self.valid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
+        self.valid_train_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.valid_test_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path: str = os.path.join(
+            self.data_validation_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+        )
 
-              self.valid_train_file_path: str = os.path.join(self.valid_data_dir,training_pipeline.TRAIN_FILE_NAME)
 
-              self.valid_test_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TEST_FILE_NAME)
-
-              self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir,training_pipeline.TRAIN_FILE_NAME)
-
-              self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir,training_pipeline.TEST_FILE_NAME)
-
-              self.drift_report_file_path: str = os.path.join(
-                self.data_validation_dir,
-                training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
-                training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME
-              )
 
 
 class DataTransformationConfig:
-        def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-            self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME)
-            
-            self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"))
-            
-            self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,  training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,training_pipeline.TEST_FILE_NAME.replace("csv", "npy") )
-            
-            self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,training_pipeline.PREPROCSSING_OBJECT_FILE_NAME)
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
+        self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,  training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            training_pipeline.PREPROCSSING_OBJECT_FILE_NAME,)
+
+
 
 class ModelTrainerConfig:
 
@@ -76,11 +77,12 @@ class ModelTrainerConfig:
         self.expected_accuracy: float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE
         self.overfitting_underfitting_threshold = training_pipeline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD
 
+
 class ModelEvaluationConfig:
 
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.model_evaluation_dir: str = os.path.join(
-            training_pipeline_config.artifact_dir,training_pipeline.MODEL_EVALUATION_DIR_NAME
+            training_pipeline_config.artifact_dir, training_pipeline.MODEL_EVALUATION_DIR_NAME
         )
         self.report_file_path = os.path.join(self.model_evaluation_dir,training_pipeline.MODEL_EVALUATION_REPORT_NAME)
         self.change_threshold = training_pipeline.MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
@@ -89,7 +91,9 @@ class ModelEvaluationConfig:
 class ModelPusherConfig:
 
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-        self.model_evaluation_dir: str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.MODEL_PUSHER_DIR_NAME)
+        self.model_evaluation_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.MODEL_PUSHER_DIR_NAME
+        )
         self.model_file_path = os.path.join(self.model_evaluation_dir,training_pipeline.MODEL_FILE_NAME)
         timestamp = round(datetime.now().timestamp())
         self.saved_model_path=os.path.join(
